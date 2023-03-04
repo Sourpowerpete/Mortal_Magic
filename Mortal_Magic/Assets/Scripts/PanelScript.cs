efb55454258a4c5a04482cf2091c5fd0607d9b9d
Sour <petermarch64@gmail.com>
2023-03-04 15:47:21 -0500

    Auto positions panelObjects
    Panels will attempt to position their contained object based on the size of the sprite bounding boxes

--- Mortal_Magic/Assets/Scripts/PanelScript.cs
+++ Mortal_Magic/Assets/Scripts/PanelScript.cs
@@ -11,7 +11,7 @@ public class PanelScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        containedGameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+        containedGameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - this.GetComponent<SpriteRenderer>().bounds.size.y / 3 + containedGameObject.GetComponent<SpriteRenderer>().bounds.size.y / 2, this.gameObject.transform.position.z);
     }
 
     // Update is called once per frame
