cd8f753bab66c49f828b35bbddd52c0884b94b1b
Sour <petermarch64@gmail.com>
2023-07-15 17:20:39 -0400

    Haven't updated in months
    Unsure of changes

---
+++ Mortal_Magic/Assets/PlayerPanelScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPanelScript : MonoBehaviour
+{
+    public Sprite[] yLevelSprites = new Sprite[3];
+    public SpriteRenderer mySpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mySpriteRenderer.sprite = yLevelSprites[2];
+    }
+}
